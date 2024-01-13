b41246e6d8186024b0ca7a57415cf8affbb0f731
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2024-01-13 13:50:29 +0000

    update

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import type {LinksFunction, LoaderFunctionArgs} from '@remix-run/node'
-import {json} from '@remix-run/node'
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,131 +7,27 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
-} from '@remix-run/react'
-import {lazy, Suspense} from 'react'
+} from "@remix-run/react";
 
-import {Layout} from '~/components/Layout'
-import {themePreferenceCookie} from '~/cookies'
-import {getBodyClassNames} from '~/lib/getBodyClassNames'
-import {useQuery} from '~/sanity/loader'
-import {loadQuery} from '~/sanity/loader.server'
-import {frontendUrl, isStegaEnabled, studioUrl} from '~/sanity/projectDetails'
-import {HOME_QUERY} from '~/sanity/queries'
-import styles from '~/tailwind.css'
-import type {HomeDocument} from '~/types/home'
-import {homeZ} from '~/types/home'
-import {themePreference} from '~/types/themePreference'
-
-const VisualEditing = lazy(() => import('~/components/VisualEditing'))
-
-export const links: LinksFunction = () => {
-  return [
-    {rel: 'stylesheet', href: styles},
-    {rel: 'preconnect', href: 'https://cdn.sanity.io'},
-    {
-      rel: 'preconnect',
-      href: 'https://fonts.gstatic.com',
-      crossOrigin: 'anonymous',
-    },
-    {
-      rel: 'preconnect',
-      href: 'https://fonts.googleapis.com',
-      crossOrigin: 'anonymous',
-    },
-    {
-      href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@500;700&family=Inter:wght@500;700;800&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap',
-      rel: 'stylesheet',
-    },
-  ]
-}
-
-export type Loader = typeof loader
-
-export const loader = async ({request}: LoaderFunctionArgs) => {
-  const stegaEnabled = isStegaEnabled(request.url)
-
-  // Dark/light mode
-  const cookieHeader = request.headers.get('Cookie')
-  const cookieValue = (await themePreferenceCookie.parse(cookieHeader)) || {}
-  const theme = themePreference.parse(cookieValue.themePreference) || 'light'
-  const bodyClassNames = getBodyClassNames(theme)
-
-  // Sanity content reused throughout the site
-  const initial = await loadQuery<HomeDocument>(
-    HOME_QUERY,
-    {},
-    {
-      perspective: stegaEnabled ? 'previewDrafts' : 'published',
-    },
-  ).then((res) => ({
-    ...res,
-    data: res.data ? homeZ.parse(res.data) : undefined,
-  }))
-
-  return json({
-    initial,
-    query: HOME_QUERY,
-    params: {},
-    theme,
-    bodyClassNames,
-    sanity: {
-      isStudioRoute: new URL(request.url).pathname.startsWith('/studio'),
-      stegaEnabled,
-    },
-    ENV: {
-      SANITY_STUDIO_PROJECT_ID: process.env.SANITY_STUDIO_PROJECT_ID!,
-      SANITY_STUDIO_DATASET: process.env.SANITY_STUDIO_DATASET!,
-      SANITY_STUDIO_API_VERSION: process.env.SANITY_STUDIO_API_VERSION!,
-      // URL of the Frontend that will be loaded into Presentation
-      SANITY_FRONTEND_URL: frontendUrl,
-      // URL of the Studio to allow requests from Presentation
-      SANITY_STUDIO_URL: studioUrl,
-    },
-  })
-}
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
 
 export default function App() {
-  const {initial, query, params, theme, bodyClassNames, sanity, ENV} =
-    useLoaderData<typeof loader>()
-  const {data, loading} = useQuery<typeof initial.data>(query, params, {
-    // @ts-expect-error
-    initial,
-  })
-
   return (
     <html lang="en">
       <head>
-        <Meta />
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href="https://fav.farm/🤘" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
         <Links />
       </head>
-      <body className={bodyClassNames}>
-        {sanity.isStudioRoute ? (
-          <Outlet />
-        ) : (
-          <>
-            <Layout home={loading || !data ? initial.data : data} theme={theme}>
-              <Outlet />
-            </Layout>
-          </>
-        )}
+      <body>
+        <Outlet />
         <ScrollRestoration />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.ENV = ${JSON.stringify(ENV)}`,
-          }}
-        />
         <Scripts />
         <LiveReload />
-        {!sanity.isStudioRoute && sanity.stegaEnabled ? (
-          <Suspense>
-            <VisualEditing studioUrl={ENV.SANITY_STUDIO_URL} />
-          </Suspense>
-        ) : null}
       </body>
     </html>
-  )
+  );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,66 +1,41 @@
-import type {LoaderFunctionArgs, MetaFunction} from '@remix-run/node'
-import {json} from '@remix-run/node'
-import {useLoaderData} from '@remix-run/react'
+import type { MetaFunction } from "@remix-run/node";
 
-import {Records} from '~/components/Records'
-import type {Loader as RootLoader} from '~/root'
-import {useQuery} from '~/sanity/loader'
-import {loadQuery} from '~/sanity/loader.server'
-import {isStegaEnabled} from '~/sanity/projectDetails'
-import {RECORDS_QUERY} from '~/sanity/queries'
-import type {RecordStub} from '~/types/record'
-import {recordStubsZ} from '~/types/record'
-
-export const meta: MetaFunction<
-  typeof loader,
-  {
-    root: RootLoader
-  }
-> = ({matches}) => {
-  const rootData = matches.find((match) => match.id === `root`)?.data
-  const home = rootData ? rootData.initial.data : null
-  const title = [home?.title, home?.siteTitle].filter(Boolean).join(' | ')
-
-  return [{title}]
-}
-
-export const loader = async ({request}: LoaderFunctionArgs) => {
-  const stegaEnabled = isStegaEnabled(request.url)
-  const initial = await loadQuery<RecordStub[]>(
-    RECORDS_QUERY,
-    {},
-    {
-      perspective: stegaEnabled ? 'previewDrafts' : 'published',
-    },
-  ).then((res) => ({
-    ...res,
-    data: res.data ? recordStubsZ.parse(res.data) : null,
-  }))
-
-  if (!initial.data) {
-    throw new Response('Not found', {status: 404})
-  }
-
-  return json({
-    initial,
-    query: RECORDS_QUERY,
-    params: {},
-  })
-}
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
 
 export default function Index() {
-  const {initial, query, params} = useLoaderData<typeof loader>()
-  const {data, loading} = useQuery<typeof initial.data>(query, params, {
-    initial,
-  })
-
-  if (loading || !data) {
-    return <div>Loading...</div>
-  }
-
   return (
-    <div className="grid grid-cols-1 gap-6 lg:gap-12">
-      <Records records={data} />
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
